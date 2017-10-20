c2e0253a903e6d254e73724b30ee1040427cba2b
LypGitHub <657993703@qq.com>
2017-10-20 12:37:17 +0800

    :arrow_up: build: add pre-commit

--- src/common/constant.js
+++ src/common/constant.js
@@ -1,3 +1,2 @@
 export const path = process.env.NODE_ENV === 'production' ? 'http://hrhg.integrity.com.cn/' : 'http://test.integrity.com.cn/'
-console.log(process.env.NODE_ENV) 
-
+console.log(process.env.NODE_ENV)
